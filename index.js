5d8557b13e328497dd0fa048972b954708c6367a
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-19 10:32:46 +0800

    update

---
+++ index.js
@@ -0,0 +1,141 @@
+// todo 用更好的方式继承 app.response.
+
+var response = require('./lib/response.js');
+var swig = require('yog-swig');
+var path = require('path');
+
+var view = module.exports = function(options) {
+    return function(req, res, next) {
+
+        var origin = res.__proto__;
+
+        res.__proto__ = response;
+        response.__proto__ = origin;
+        origin = null;
+
+        next();
+    }
+};
+
+view.engines = {
+    'swig': swig
+}
+
+view.create = function(settings, app) {
+    var Engine;
+
+    settings = settings || {};
+
+    if (app) {
+        settings.views = app.get('views');
+    }
+
+    Engine = view.engines[settings.engine];
+
+    return function(filepath, options, done) {
+        var res = options.response;
+        var bigpipe = res.bigpipe;
+
+        if (!res.fis) {
+            throw new Error('Fis Resource middleware is required!')
+            done('error', 'fis is required!');
+        }
+
+        var fis = res.fis;
+        var prototols = createHanlder(fis, bigpipe, settings.views);
+        var engine = new Engine(settings);
+        var bufs = [];
+        var flush = function() {
+            while((d = bufs.shift())) {
+                d = fis.filter(d);
+                res.write(d);
+            }
+        };
+
+        engine.on('data', function(d) {
+            bufs.push(d);
+        });
+
+        engine.on('flush', flush);
+
+        engine.on('end', function(output) {
+            output = output || bufs.pop();
+
+            var identify = '</body>';
+            var idx = output.indexOf(identify);
+            var clouser = '';
+
+            engine.removeAllListeners();
+
+            if (~idx && bigpipe) {
+                idx += identify.length;
+                clouser = output.substring(idx);
+                output = output.substring(0, idx);
+
+                bufs.push(output);
+                flush();
+
+                bigpipe.render(res, function() {
+                    bufs.push(clouser);
+                    flush();
+                    done();
+                });
+
+                return;
+            }
+
+            bufs.push(output);
+            done();
+        });
+
+        engine.on('error', function(reason) {
+            engine.removeAllListeners();
+            done(reason);
+        });
+
+        options._yog = prototols;
+        engine.renderFile(filepath, options);
+    }
+};
+
+// 创建一个处理器给模板引擎使用。
+function createHanlder(fis, bigpipe, views) {
+    var api = {};
+
+    ['addScript', 'addStyle', 'load', 'getUri'].forEach(function(key) {
+        api[key] = function() {
+            return fis[key].apply(fis, arguments);
+        }
+    });
+
+    bigpipe && ['addPagelet'].forEach(function(key) {
+        api[key] = function() {
+            return bigpipe[key].apply(bigpipe, arguments);
+        }
+    });
+
+    api.resolve = function(id) {
+        return path.join(views, fis.getUri(id));
+    };
+
+    api.setFramework = function(js) {
+        fis.framework = js;
+    };
+
+    api.supportBigPipe = function() {
+        return !!bigpipe;
+    };
+
+    api.fork = function(fis, bigpipe, views) {
+        views = views || api.views;
+
+        var forked = createHanlder(fis, bigpipe, views);
+        return forked;
+    };
+
+    api.fis = fis;
+    api.bigpipe = bigpipe;
+    api.views = views;
+
+    return api;
+}
