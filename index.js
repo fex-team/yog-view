2e9c2467800b94e03f4b40a880eb988f601a39eb
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-25 16:36:09 +0800

    optimize

--- index.js
+++ index.js
@@ -3,162 +3,119 @@
 var response = require('./lib/response.js');
 var swig = require('yog-swig');
 var path = require('path');
+var api = require('./lib/api.js');
+var hacked = false;
 
-var view = module.exports = function(options) {
-    return function(req, res, next) {
+var view = module.exports = {
+    engines: {
+        'swig': swig,
+    },
 
+    create: function(settings, app) {
+        var Engine;
+
+        if (arguments.length === 1) {
+            app = settings;
+            settings = {};
+        }
+
+        hackResponse(app);
+
+        settings.views = app.get('views');
+
+        Engine = view.engines[settings.engine || 'swig'];
+
+        return function(filepath, options, done) {
+            
+            var res = options.response;
+            var bigpipe = res.bigpipe;
+            var fis = res.fis;
+            var prototols = api(fis, bigpipe, settings.views);
+            var engine = new Engine(settings, prototols);
+            
+            var bufs = [];
+
+            // 这个模式表示是一次请求局部内容的请求。
+            // 不需要把框架吐出来了。
+            // 只需输出 mode="quicking" 的 widget.
+            var isQuickingMode = bigpipe && bigpipe.isQuickingMode();
+            var flush = function() {
+                if (isQuickingMode) {
+                    return;
+                }
+
+                while((d = bufs.shift())) {
+                    d = prototols.filter(d);
+                    res.write(d);
+                }
+            };
+
+            var finish = function(err, data) {
+                engine.destroy();
+                options.response = engine = bigpipe = fis = null;
+
+                if (err) {
+                    return done(err);
+                }
+
+                bufs.push(data || '');
+                flush();
+                res = null;
+                done();
+            };
+
+            engine.on('data', function(d) {
+                bufs.push(d);
+            });
+
+            engine.on('flush', flush);
+
+            engine.on('end', function(output) {
+                output = output || bufs.pop() || '';
+
+                var identify = '</body>';
+                var idx = output.indexOf(identify);
+                var clouser = '';
+                
+                // bigpipe mode
+                if (bigpipe && (~idx || isQuickingMode)) {
+                    
+                    if (~idx) {
+                        idx += identify.length;
+                        clouser = output.substring(idx);
+                        output = output.substring(0, idx);
+                    }
+
+                    // flush framework.
+                    bufs.push(output);
+                    flush();
+
+                    // then chunk out pagelets
+                    return bigpipe.render(res, finish.bind(this, null, clouser));
+                }
+
+                // otherwise 
+                finish(null, output);
+            });
+
+            engine.on('error', finish);
+            options._yog = prototols;
+            engine.renderFile(filepath, options);
+        }
+    }
+};
+
+// hack into response class.
+function hackResponse(app) {
+    if (hacked) return;
+    hacked = true;
+
+    app.use(function hackResponse(req, res, next) {
         var origin = res.__proto__;
-
-        res.__proto__ = response;
         response.__proto__ = origin;
+        res.__proto__ = response;
         origin = null;
 
         next();
-    }
-};
-
-view.engines = {
-    'swig': swig
-}
-
-view.create = function(settings, app) {
-    var Engine;
-
-    settings = settings || {};
-
-    if (app) {
-        settings.views = app.get('views');
-    }
-
-    Engine = view.engines[settings.engine];
-
-    return function(filepath, options, done) {
-        var res = options.response;
-        var bigpipe = res.bigpipe;
-
-        if (!res.fis) {
-            throw new Error('Fis Resource middleware is required!')
-            done('error', 'fis is required!');
-        }
-
-        var fis = res.fis;
-        var prototols = createHanlder(fis, bigpipe, settings.views);
-        var engine = new Engine(settings, prototols);
-        var bufs = [];
-
-        // 这个模式表示是一次请求局部内容的请求。
-        // 不需要把框架吐出来了。
-        // 只需输出 mode="quicking" 的 widget.
-        var isQuickingMode = bigpipe.isQuickingMode();
-        var flush = function() {
-            if (isQuickingMode) {
-                return;
-            }
-
-            while((d = bufs.shift())) {
-                d = fis.filter(d);
-                res.write(d);
-            }
-        };
-
-        engine.on('data', function(d) {
-            bufs.push(d);
-        });
-
-        engine.on('flush', flush);
-
-        engine.on('end', function(output) {
-            output = output || bufs.pop() || '';
-
-            var identify = '</body>';
-            var idx = output.indexOf(identify);
-            var clouser = '';
-
-            engine.removeAllListeners();
-
-            if (bigpipe && (~idx || isQuickingMode)) {
-                if (~idx) {
-                    idx += identify.length;
-                    clouser = output.substring(idx);
-                    output = output.substring(0, idx);
-                }
-                bufs.push(output);
-                flush();
-
-                bigpipe.render(res, function() {
-                    bufs.push(clouser);
-                    flush();
-                    done();
-                });
-
-                return;
-            }
-
-            bufs.push(output);
-            flush();
-            done();
-        });
-
-        engine.on('error', function(reason) {
-            engine.removeAllListeners();
-            done(reason);
-        });
-
-        options._yog = prototols;
-        engine.renderFile(filepath, options);
-    }
-};
-
-// 创建一个处理器给模板引擎使用。
-function createHanlder(fis, bigpipe, views) {
-    var api = {};
-
-    ['addScript', 'addStyle', 'load', 'getUri'].forEach(function(key) {
-        api[key] = function() {
-            return fis[key].apply(fis, arguments);
-        }
     });
-
-    bigpipe && ['addPagelet'].forEach(function(key) {
-        api[key] = function() {
-            return bigpipe[key].apply(bigpipe, arguments);
-        }
-    });
-
-    api.resolve = function(id) {
-        if (!~id.indexOf(':')) {
-            return id;
-        }
-        return path.join(views, fis.getUri(id));
-    };
-
-    api.setFramework = function(js) {
-        this.fis.framework = js;
-    };
-
-    api.supportBigPipe = function() {
-        return !!bigpipe;
-    };
-
-    api.fork = function(fis, bigpipe, views) {
-        if (arguments.length === 0 ) {
-            fis = this.fis;
-            bigpipe = this.bigpipe;
-            views = this.views;
-        } else if (arguments.length === 1) {
-            bigpipe = this.bigpipe;
-            views = this.views;
-        } else if (arguments.length === 2) {
-            views = this.views;
-        }
-
-        var forked = createHanlder(fis, bigpipe, views);
-        return forked;
-    };
-
-    api.fis = fis;
-    api.bigpipe = bigpipe;
-    api.views = views;
-
-    return api;
 }
