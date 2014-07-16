7afeaee172f26690f9c9035acc9a441bbba30bad
liaoxuezhi <liaoxuezhi@baidu.com>
2014-07-16 14:13:56 +0800

    去除对 yog-swig 的依赖

--- index.js
+++ index.js
@@ -1,87 +1,77 @@
-// todo 用更好的方式继承 app.response.
-
 var response = require('./lib/response.js');
-var swig = require('yog-swig');
-var path = require('path');
 var layer = require('./lib/layer.js');
 var combine = require('./lib/combine.js');
-var hacked = false;
+var _ = require('./lib/util.js');
 
-var view = module.exports = {
-    engines: {
-        'swig': swig,
-    },
 
-    create: function(settings, app) {
-        var Engine;
+exports.init = function(settings, app) {
+    var Engine;
 
-        if (arguments.length === 1) {
-            app = settings;
-            settings = {};
-        }
+    if (arguments.length === 1) {
+        app = settings;
+        settings = {};
+    }
 
-        // 让 response.render 的时候，将 response 实例作为 locals 参数携带进来。
-        hackResponse(app);
+    // 让 response.render 的时候，将 response 实例作为 locals 参数携带进来。
+    hackResponse(app);
 
-        settings.views = app.get('views');
-        Engine = view.engines[settings.engine || 'swig'];
+    settings.views = app.get('views');
+    Engine = _.resolveEngine(settings.engine || 'yog-swig');
 
-        return function(filepath, locals, done) {
+    return function(filepath, locals, done) {
+        
+        // 关于 response 来源，请查看 hackResponse 方法。
+        // 以及 lib/reponse.js
+        var res = locals.response;
+
+        // 创建一个新对象。
+        var options = _.mixin({}, settings);
+        
+        // 初始化 layer 层。
+        // 提供 addScript, addStyle, resolve, addPagelet 各种接口。
+        // 用来扩展模板层能力。
+        var prototols = layer(res, settings);
+
+        var sentData = false;
+
+        // 模本文件路径
+        options.view = filepath;
+
+        // 模板变量。
+        // locals._yog 用来指向 layer 层。
+        options.locals = _.mixin(locals, {_yog: prototols});
+
+        new Engine(options, prototols)
             
-            // 关于 response 来源，请查看 hackResponse 方法。
-            // 以及 lib/reponse.js
-            var res = locals.response;
+            // 合并 tpl 流 和 bigpipe 流。
+            .pipe(combine(prototols))
 
-            // 创建一个新对象。
-            var options = mixin({}, settings);
-            
-            // 初始化 layer 层。
-            // 提供 addScript, addStyle, resolve, addPagelet 各种接口。
-            // 用来扩展模板层能力。
-            var prototols = layer(res, settings);
+            .on('data', function() {
+                sentData = true;
+            })
 
-            // 模本文件路径
-            options.view = filepath;
+            .on('error', function(error) {
+                // 属于 chunk error
+                if (sentData) {
+                    if (typeof settings.chunkErrorHandler === 'function') {
+                        settings.chunkErrorHandler(error, res);
+                    } else {
+                        res.write('<script>window.console && console.error("chunk error", "'+ error.message.replace(/"/g, "\\\"") +'")</script>');
+                    }
+                    res.end();
+                } else {
+                    // 交给 express 去处理错误吧。
+                    done(error);
+                }
+            })
 
-            // 模板变量。
-            // locals._yog 用来指向 layer 层。
-            options.locals = mixin(locals, {_yog: prototols});
-
-            var tpl = new Engine(options, prototols);
-
-            tpl
-                // 合并 tpl 流 和 bigpipe 流。
-                .pipe(combine(prototols))
-
-                // 直接输出到 response.
-                .pipe(res);
-
-                // 如果不需要调用 done 方法，可以直接 .pipe(res);
-                // .on('data', function(chunk) {
-                //     res.write(chunk);
-                // })
-
-                // .on('end', function() {
-                //     done();
-                // })
-
-                // .on('error', function(reason) {
-                //     done(reason || 'tpl error!');
-                // });
-        }
+            // 直接输出到 response.
+            .pipe(res);
     }
 };
 
-function mixin(a, b) {
-    if (a && b) {
-        for (var key in b) {
-            a[key] = b[key];
-        }
-    }
-    return a;
-}
-
 // hack into response class.
+var hacked = false;
 function hackResponse(app) {
     if (hacked) return;
     hacked = true;
