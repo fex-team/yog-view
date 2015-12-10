857e6acff0d8cdb53787c8ec6ee0355cc32fefb3
hefangshi <hefangshi@gmail.com>
2015-12-10 18:15:01 +0800

    excute all sources in spider mode

--- index.js
+++ index.js
@@ -24,50 +24,62 @@ yogViewEngine.prototype.cleanCache = function () {
 yogViewEngine.prototype.renderFile = function (filepath, locals, done) {
     var res = locals.__res__;
     var settings = this.settings;
-
+    var me = this;
     // 初始化 layer 层。
     // 提供 addScript, addStyle, resolve, addPagelet 各种接口。
     // 用来扩展模板层能力。
     var prototols = layer(res, settings);
 
-    var sentData = false;
+    if (prototols.bigpipe.isSpiderMode) {
+        // 获取所有异步pagelet信息
+        prototols.bigpipe.prepareAllSources(function (data) {
+            render();
+        });
+    }
+    else {
+        render();
+    }
 
-    this.engine.makeStream(filepath, _.mixin(locals, {
-            _yog: prototols
-        }))
-        // 合并 tpl 流 和 bigpipe 流。
-        .pipe(combine(prototols))
-        // 设置默认content-type
-        .on('data', function () {
-            sentData = true;
-            if (!res.get('Content-Type')) {
-                res.type('html');
-            }
-            setImmediate(function () {
-                res.flush();
-            });
-        })
-        // bigpipe异步回调异常
-        .on('error', function (error) {
-            // 属于 chunk error
-            if (sentData) {
-                if (typeof settings.chunkErrorHandler === 'function') {
-                    settings.chunkErrorHandler(error, res);
+    function render() {
+        var sentData = false;
+
+        me.engine.makeStream(filepath, _.mixin(locals, {
+                _yog: prototols
+            }))
+            // 合并 tpl 流 和 bigpipe 流。
+            .pipe(combine(prototols))
+            // 设置默认content-type
+            .on('data', function () {
+                sentData = true;
+                if (!res.get('Content-Type')) {
+                    res.type('html');
+                }
+                setImmediate(function () {
+                    res.flush();
+                });
+            })
+            // bigpipe异步回调异常
+            .on('error', function (error) {
+                // 属于 chunk error
+                if (sentData) {
+                    if (typeof settings.chunkErrorHandler === 'function') {
+                        settings.chunkErrorHandler(error, res);
+                    }
+                    else {
+                        res.write('<script>window.console && console.error("chunk error", "' + error.message.replace(
+                            /"/g,
+                            '\\\"') + '")</script>');
+                    }
+                    res.end();
                 }
                 else {
-                    res.write('<script>window.console && console.error("chunk error", "' + error.message.replace(
-                        /"/g,
-                        '\\\"') + '")</script>');
+                    // 模板渲染前报错，传递至next
+                    done(error);
                 }
-                res.end();
-            }
-            else {
-                // 模板渲染前报错，传递至next
-                done(error);
-            }
-        })
-        // 直接输出到 response.
-        .pipe(res);
+            })
+            // 直接输出到 response.
+            .pipe(res);
+    }
 };
 
 module.exports = yogViewEngine;
